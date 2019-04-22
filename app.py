b6833a86bb2acb574ed5b82063528f2706286b76
Tomoya Furukawa <ft@furukawatomoyanoMacBook-puro.local>
2019-04-22 18:05:22 +0900

    b

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import json
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 
 err1 = {"message": "Account creation failed", "cause": "required user_id and password"}
 
-app = Flask(__name__)
+users = []
 
+app = Flask(__name__)
 
 @app.route("/")
 def return404():
@@ -18,8 +19,10 @@ def return404():
 def callback():
     l = request.json
 
-    if "user_id" not in l or "" not in l:
-        abort(400, err1)
+    if "user_id" not in l or "nickname" not in l:
+        abort(400)
+        return jsonify(err1)
+    
     return str(l)
 
 
